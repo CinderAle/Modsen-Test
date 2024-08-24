66fcc6e95e2da996f6fa48837ccedd9bfa262bb8
CinderAle <a.d.strukov@gmail.com>
2024-08-24 15:43:50 +0300

    fix: updated prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
+import { FormEvent } from 'react';
+
 function App() {
-  return (
-    <>
-      <div>Hello</div>
-    </>
-  );
+    const e = (a: FormEvent) => {
+        a.preventDefault();
+    };
+    const [a, setA] = useState(0);
+    return (
+        <form onSubmit={e}>
+            <div onClick={() => setA(1)}>{a}</div>
+        </form>
+    );
 }
 
 export default App;
