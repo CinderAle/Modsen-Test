febe12af2a1166862ddb1454f02a9057d35945c1
CinderAle <a.d.strukov@gmail.com>
2024-08-29 14:57:21 +0300

    feat: added Loader component with styles

---
+++ src/components/Loader/styles/index.ts
@@ -0,0 +1,16 @@
+import styled, { keyframes } from 'styled-components';
+
+const rotateAnimation = keyframes`
+    100%{transform: rotate(1turn)}
+`;
+
+export const Spinner = styled.div`
+    width: 50px;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    background:
+        radial-gradient(farthest-side, #ffa516 94%, #0000) top/8px 8px no-repeat,
+        conic-gradient(#0000 30%, #ffa516);
+    -webkit-mask: radial-gradient(farthest-side, #0000 calc(100% - 8px), #000 0);
+    animation: ${rotateAnimation} 1s infinite linear;
+`;
