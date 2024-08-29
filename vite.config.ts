f823c0f740369f466458aa9bc28cb7b1837c4824
CinderAle <a.d.strukov@gmail.com>
2024-08-29 21:01:23 +0300

    fix: updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,4 @@ export default defineConfig({
     resolve: {
         alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
     },
-    build: {
-        outDir: './build',
-        emptyOutDir: true,
-    },
 });
